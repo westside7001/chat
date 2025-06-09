bbe71ace6eb70975f87505655010f37f5874e27c
Curtis Hsu <westside7001@gmail.com>
2025-06-09 15:57:47 -0700

    從input 轉換成 output

--- chat.py
+++ chat.py
@@ -3,40 +3,47 @@ from fileinput import filename
 
 #讀取檔案
 def read_file(filename):
-    products = []
-    with open(filename, 'r', encoding='utf-8') as f:
+    chat = []
+    with open(filename, 'r', encoding='utf-8-sig') as f:
         for line in f:
-            #if '商品,價格' in line:
-            #    continue  # 遇到"商品,價格"字串，則跳過此行
-            chat = line.strip().split(',')
-            products.append([chat])
-    return products
+            chat.append(line.strip())
+    return chat
+
+def convert(chats):
+    new = []
+    person = None
+    for gpt in chats:
+        if gpt == 'Allen':
+            person = "Allen"
+            continue
+        elif gpt == 'Tom':
+            person = 'Tom'
+            continue
+        if person:
+            new.append(person + ': '+gpt)
+    return new
+
+def write_file(filename, chat):
+    with open(filename,'w') as f:
+        for line in chat:
+            f.write(line + '\n')
+
+
+def main():
+    chats = read_file('input.txt')
+    chats = convert(chats)
+    write_file('output.txt',chats)
+
+
+main()
+
 
 #讀取檔案
 
 #印出所有購買記錄
-def print_products(products):
-    for p in products:
-        print(p[0])
-
-#寫入檔案
-#def write_file(filename, products):
-#    with open(filename,'w', encoding='utf-8') as f:
-#        f.write('商品,價格\n')
-#        for p in products:
-#            f.write(p[0]+','+p[1]+'\n')
-
-#def main():
-#    filename = 'products.csv'
-#    if os.path.isfile(filename): #檢查檔案是否存在
-#        print('file exist')
-#        products = read_file(filename)
+#def print_products(products):
+#    for p in products:
+#        print(p)
 #
-#    else:
-#        print('file is not exist')
-#
-#    products = user_input(products)
-#    print_products(products)
-#    write_file('products.csv', products)
 
-#main()
+
